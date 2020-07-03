6da6b9245d45478964fb02f3bbf64047b34fe77e
Chisato Murakami <kiwipom1031@gmail.com>
2020-07-03 10:49:02 -0700

    add showform in KegControl

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,29 +1,40 @@
-import React from 'react'
-import KegList from './KegList'
-import KegForm from './KegForm'
+import React from "react";
+import KegList from "./KegList";
+import KegForm from "./KegForm";
 
 class KegControl extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      masterKegList: [{name: "fake keg", brand: "fake brand", price: 10, alcoholContent: 5}, {name: "fake keg2", brand: "fake brand", price: 10, alcoholContent: 5}]
-    }
+      masterKegList: [
+        { name: "fake keg", brand: "fake brand", price: 10, alcoholContent: 5 },
+        {
+          name: "fake keg2",
+          brand: "fake brand",
+          price: 10,
+          alcoholContent: 5,
+        },
+      ],
+      showForm: false,
+    };
   }
 
   handleAddingNewKegToList = (newKeg) => {
     const newMasterKegList = this.state.masterKegList.concat(newKeg);
-    this.setState({masterKegList: newMasterKegList});
-  } //[]updating with a new keg, array of kegs
+    this.setState({ masterKegList: newMasterKegList });
+  }; //[]updating with a new keg, array of kegs
 
-  
   render() {
-    return (
-      <React.Fragment>
-        <KegList kegList = {this.state.masterKegList}/>
-        <KegForm onNewKegCreation={this.handleAddingNewKegToList}/>
-      </React.Fragment>
-    )
+    let currentPage = null;
+    if (this.state.showForm) {
+      currentPage = (
+        <KegForm onNewKegCreation={this.handleAddingNewKegToList} />
+      );
+    } else {
+      currentPage = <KegList kegList={this.state.masterKegList} />;
+    }
+    return <React.Fragment>{currentPage}</React.Fragment>;
   }
 }
 
-export default KegControl
+export default KegControl;
